feca6611e866026f23f9737a607d1246fb2883f7
Dreamlu <596392912@qq.com>
2015-12-21 11:33:19 +0800

    #37

--- src/com/jfinal/weixin/sdk/api/SnsAccessTokenApi.java
+++ src/com/jfinal/weixin/sdk/api/SnsAccessTokenApi.java
@@ -10,6 +10,7 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.Callable;
 
+import com.jfinal.kit.StrKit;
 import com.jfinal.weixin.sdk.kit.ParaMap;
 import com.jfinal.weixin.sdk.kit.PaymentKit;
 import com.jfinal.weixin.sdk.utils.HttpUtils;
@@ -31,6 +32,18 @@ public class SnsAccessTokenApi
      * @return url
      */
     public static String getAuthorizeURL(String appId, String redirect_uri, boolean snsapiBase) {
+        return getAuthorizeURL(appId, redirect_uri, null, snsapiBase);
+    }
+    
+    /**
+     * 生成Authorize链接
+     * @param appId 应用id
+     * @param redirect_uri 回跳地址
+     * @param state 重定向后会带上state参数，开发者可以填写a-zA-Z0-9的参数值，最多128字节
+     * @param snsapiBase snsapi_base（不弹出授权页面，只能拿到用户openid）snsapi_userinfo（弹出授权页面，这个可以通过 openid 拿到昵称、性别、所在地）
+     * @return url
+     */
+    public static String getAuthorizeURL(String appId, String redirect_uri, String state, boolean snsapiBase) {
         Map<String, String> params = new HashMap<String, String>();
         params.put("appid", appId);
         params.put("response_type", "code");
@@ -42,7 +55,11 @@ public class SnsAccessTokenApi
         } else {
             params.put("scope", "snsapi_userinfo");
         }
-        params.put("state", "wx#wechat_redirect");
+        if (StrKit.isBlank(state)) {
+            params.put("state", "wx#wechat_redirect");
+        } else {
+        	params.put("state", state.concat("#wechat_redirect"));
+        }
         String para = PaymentKit.packageSign(params, false);
         return authorize_uri + "?" + para;
     }
