bb98f070c114d981e6089737590c82c2cd75c172
孤独的√3 <596392912@qq.com>
2016-01-15 22:58:18 +0800

    test

--- test/com/jfinal/weixin/sdk/msg/OutMsgTest.java
+++ test/com/jfinal/weixin/sdk/msg/OutMsgTest.java
@@ -5,7 +5,10 @@ import org.junit.Test;
 
 import com.jfinal.weixin.sdk.msg.in.InMsg;
 import com.jfinal.weixin.sdk.msg.in.InTextMsg;
+import com.jfinal.weixin.sdk.msg.out.OutCustomMsg;
 import com.jfinal.weixin.sdk.msg.out.OutTextMsg;
+import com.jfinal.weixin.sdk.msg.out.OutVideoMsg;
+import com.jfinal.weixin.sdk.msg.out.OutVoiceMsg;
 
 public class OutMsgTest {
 
@@ -17,8 +20,57 @@ public class OutMsgTest {
 		msg.setCreateTime(msg.now());
 		msg.setContent("jfinal weixin 极速开发平台碉堡了");
 		
+		System.out.println();
+		System.out.println(msg.toXml());
 		InMsg inMsg = InMsgParser.parse(msg.toXml());
 		Assert.assertTrue(inMsg instanceof InTextMsg);
+	}
+	
+	@Test
+	public void test2() {
+		OutCustomMsg msg = new OutCustomMsg();
+		msg.setToUserName("to james");
+		msg.setFromUserName("from james");
+		msg.setCreateTime(msg.now());
+		
+		System.out.println();
+		System.out.println(msg.toXml());
+	}
+	
+	@Test
+	public void test3() {
+		OutVoiceMsg msg = new OutVoiceMsg();
+		msg.setToUserName("to james");
+		msg.setFromUserName("from james");
+		msg.setCreateTime(msg.now());
+		msg.setMediaId("media_id");
+		
+		System.out.println();
+		System.out.println(msg.toXml());
+	}
+	
+	@Test
+	public void test4() {
+		OutVideoMsg msg = new OutVideoMsg();
+		msg.setToUserName("to james");
+		msg.setFromUserName("from james");
+		msg.setCreateTime(msg.now());
+		msg.setMediaId("media_id");
+		
+		System.out.println();
+		System.out.println(msg.toXml());
+	}
+	
+	@Test
+	public void test5() {
+		OutVoiceMsg msg = new OutVoiceMsg();
+		msg.setToUserName("to james");
+		msg.setFromUserName("from james");
+		msg.setCreateTime(msg.now());
+		msg.setMediaId("media_id");
+		
+		System.out.println();
+		System.out.println(msg.toXml());
 	}
 	
 }
