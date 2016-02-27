cae451e5e874cb506c29d2d53e0adfbae215682a
Dreamlu <596392912@qq.com>
2016-02-27 15:24:29 +0800

    添加微信红包接口

--- src/com/jfinal/weixin/sdk/api/RedPackApi.java
+++ src/com/jfinal/weixin/sdk/api/RedPackApi.java
@@ -15,7 +15,6 @@ public class RedPackApi {
 	private static String sendRedPackUrl = "https://api.mch.weixin.qq.com/mmpaymkttransfers/sendredpack";
 	
 	/**
-	 * 
 	 * 发送红包
 	 * @param params 请求参数
 	 * @param certPath 证书文件目录
@@ -40,4 +39,17 @@ public class RedPackApi {
 		return HttpUtils.postSSL(getHBInfo, PaymentKit.toXml(params), certPath, partner);
 	}
 
+	// 裂变红包：https://pay.weixin.qq.com/wiki/doc/api/cash_coupon.php?chapter=16_5
+	private static String sendGroupRedPackUrl = "https://api.mch.weixin.qq.com/mmpaymkttransfers/sendgroupredpack";
+	
+	/**
+	 * 发送裂变红包
+	 * @param params 请求参数
+	 * @param certPath 证书文件目录
+	 * @param certPass 证书密码
+	 * @return
+	 */
+	public static String sendGroupRedPack(Map<String, String> params, String certPath, String partner) {
+		return HttpUtils.postSSL(sendGroupRedPackUrl, PaymentKit.toXml(params), certPath, partner);
+	}
 }
