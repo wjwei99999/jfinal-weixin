c9aeed4f1de80fdf50481bff4c424f7dd8ff87f8
Dreamlu <596392912@qq.com>
2016-03-02 18:19:51 +0800

    fix

--- src/com/jfinal/weixin/demo/RedPackApiController.java
+++ src/com/jfinal/weixin/demo/RedPackApiController.java
@@ -1,13 +1,13 @@
-﻿package com.jfinal.weixin.demo;
-
-import java.util.HashMap;
-import java.util.Map;
+package com.jfinal.weixin.demo;
 
 import com.jfinal.core.Controller;
 import com.jfinal.kit.StrKit;
 import com.jfinal.weixin.sdk.api.RedPackApi;
 import com.jfinal.weixin.sdk.kit.IpKit;
 import com.jfinal.weixin.sdk.kit.PaymentKit;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * 微信红包demo
@@ -22,7 +22,7 @@ public class RedPackApiController extends Controller {
 	//API密钥
 	private static String paternerKey = "";
 	//微信证书路径
-	private static String certPath="";
+	private static String certPath = "";
 
 	public void send() {
 		// 接受红包的用户用户在wxappid下的openid
@@ -59,24 +59,24 @@ public class RedPackApiController extends Controller {
 		//创建签名
 		String sign = PaymentKit.createSign(params, paternerKey);
 		params.put("sign", sign);
-		
+
 		String xmlResult = RedPackApi.sendRedPack(params, certPath, partner);
 		Map<String, String> result = PaymentKit.xmlToMap(xmlResult);
 		System.out.println(result);
 		//业务结果
-		String result_code = result.get("result_code"); 
+		String result_code = result.get("result_code");
 		//此字段是通信标识，非交易标识，交易是否成功需要查看result_code来判断
 		String return_code = result.get("return_code");
 		//
 		if (StrKit.isBlank(result_code) || !"SUCCESS".equals(result_code)) {
 			System.out.println("发送成功");
-		}else{
+		} else {
 			System.out.println("发送失败");
 		}
 		renderJson(result);
 	}
-	
-	public void query(){
+
+	public void query() {
 		Map<String, String> params = new HashMap<String, String>();
 		// 随机字符串
 		params.put("nonce_str", System.currentTimeMillis() / 1000 + "");
@@ -86,12 +86,12 @@ public class RedPackApiController extends Controller {
 		params.put("mch_id", partner);
 		// 公众账号ID
 		params.put("appid", wxappid);
-		params.put("bill_type",  "MCHT");
+		params.put("bill_type", "MCHT");
 		//创建签名
 		String sign = PaymentKit.createSign(params, paternerKey);
 		params.put("sign", sign);
-		
-		String xmlResult = RedPackApi.getHbInfo(params,certPath,partner);
+
+		String xmlResult = RedPackApi.getHbInfo(params, certPath, partner);
 		Map<String, String> result = PaymentKit.xmlToMap(xmlResult);
 		System.out.println(result);
 		renderJson(result);
