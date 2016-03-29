c087c313d77c965354558706488776a454e4462c
Dreamlu <596392912@qq.com>
2016-03-29 15:58:09 +0800

    update

---
+++ src/com/jfinal/weixin/sdk/utils/Charsets.java
@@ -0,0 +1,15 @@
+package com.jfinal.weixin.sdk.utils;
+
+import java.nio.charset.Charset;
+
+/**
+ * 字符集工具类
+ * Author: L.cm
+ * Date: 2016年3月29日 下午3:44:52
+ */
+public class Charsets {
+
+	// 字符集utf-8
+	public static final Charset UTF_8 = Charset.forName("utf-8");
+
+}
