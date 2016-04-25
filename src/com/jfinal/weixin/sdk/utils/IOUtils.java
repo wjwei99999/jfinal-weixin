c435965d59604a3f9e329d4a882d929f1ac8d1a2
孤独的√3 <596392912@qq.com>
2016-04-25 17:32:47 +0800

    修改字符集问题

--- src/com/jfinal/weixin/sdk/utils/IOUtils.java
+++ src/com/jfinal/weixin/sdk/utils/IOUtils.java
@@ -1,11 +1,7 @@
 package com.jfinal.weixin.sdk.utils;
 
-import java.io.Closeable;
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.*;
+import java.nio.charset.Charset;
 
 /**
  * IOUtils
@@ -29,23 +25,38 @@ public abstract class IOUtils {
 	}
 	
 	/**
-	 * InputStream to String
+	 * InputStream to String utf-8
 	 * 
 	 * @param input  the <code>InputStream</code> to read from
 	 * @return the requested String
 	 * @throws NullPointerException if the input is null
 	 * @throws IOException if an I/O error occurs
 	 */
-	public static String toString(InputStream input) throws IOException {
+	private static String toString(InputStream input) throws IOException {
+		return toString(input, Charsets.UTF_8);
+	}
+
+	/**
+	 * InputStream to String
+	 *
+	 * @param input  the <code>InputStream</code> to read from
+	 * @param charset  the <code>Charset</code>
+	 * @return the requested String
+	 * @throws NullPointerException if the input is null
+	 * @throws IOException if an I/O error occurs
+	 */
+	public static String toString(InputStream input, Charset charset) throws IOException {
+		InputStreamReader in = new InputStreamReader(input, charset);
 		StringBuffer out = new StringBuffer();
-		byte[] b = new byte[DEFAULT_BUFFER_SIZE];
-		for (int n; (n = input.read(b)) != -1;) {
-			out.append(new String(b, 0, n));
+		char[] c = new char[DEFAULT_BUFFER_SIZE];
+		for (int n; (n = in.read(c)) != -1;) {
+			out.append(new String(c, 0, n));
 		}
+		IOUtils.closeQuietly(in);
 		IOUtils.closeQuietly(input);
 		return out.toString();
 	}
-	
+
 	/**
 	 * InputStream to File
 	 * @param input  the <code>InputStream</code> to read from
