051c0f04f06998a8d5b2365747e824c1235b3893
James <jfinal@126.com>
2018-01-13 22:44:02 +0800

    jfinal weixin 2.0

--- src/main/java/com/jfinal/weixin/sdk/cache/IAccessTokenCache.java
+++ src/main/java/com/jfinal/weixin/sdk/cache/IAccessTokenCache.java
@@ -2,8 +2,8 @@ package com.jfinal.weixin.sdk.cache;
 
 public interface IAccessTokenCache {
 
-    // 默认超时时间7200秒 5秒用于程序执行误差
-    int DEFAULT_TIME_OUT = 7200 - 5;
+    // 默认超时时间7200秒 9秒用于程序执行误差
+    int DEFAULT_TIME_OUT = 7200 - 9;
 
     String get(String key);
 
